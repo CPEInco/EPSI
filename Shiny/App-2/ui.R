a6b37fdf891f35e96b2fd549fe781491bd159c7f
Christian Hovmand Petersen <cpe@in-commodities.com>
2023-11-22 14:45:36 +0100

    cleaning up

--- Shiny/App-2/ui.R
+++
@@ -1,7 +0,0 @@
-ui = function(){
-  ui <- fluidPage(
-    selectInput("PriceArea", label="PriceArea",choices=c("DE", "FR", "AT", "BE","NL", "GB", "ES", "IT", "CH", "HU", "CZ", "SK", "DK1", "DK2", "PL", "SI", "HR", "SE3", "SE4", "FI")),
-    plotlyOutput("plot"),
-    
-  )
-} 
